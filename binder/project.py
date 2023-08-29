f56ce8ab4e36dc6c360734301fc54a1291656eef
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2023-08-29 18:49:09 +0100

    patch pythonpath

--- binder/project.py
+++ binder/project.py
@@ -1,8 +1,11 @@
 from cassini import Project, DEFAULT_TIERS
 from cassini.jlgui import extend_project
+import os
 
 project = Project(DEFAULT_TIERS, __file__)
 extend_project(project)
+
+os.environ['PYTHONPATH'] = os.get('PYTHONPATH', '') + os.pathsep + os.path.pardir(__file__)
 
 if __name__ == '__main__':
     project.launch()
