8b6221c332cb7ee2bbecab3aa1ad4e94b0e82a82
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2023-08-29 19:02:06 +0100

    try setting pythonpath in start

--- binder/project.py
+++ binder/project.py
@@ -1,11 +1,8 @@
 from cassini import Project, DEFAULT_TIERS
 from cassini.jlgui import extend_project
-import os
 
 project = Project(DEFAULT_TIERS, __file__)
 extend_project(project)
-
-os.environ['PYTHONPATH'] = os.environ.get('PYTHONPATH', '') + os.pathsep + os.path.dirname(__file__)
 
 if __name__ == '__main__':
     project.launch()
