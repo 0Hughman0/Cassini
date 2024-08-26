c08ceb4106b2fff96a2be872942a47ba903ab78b
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2024-08-26 17:42:36 +0100

    started 0.2 -> 0.3 migration tool

--- cassini/compat/update.py
+++ cassini/compat/update.py
@@ -2,6 +2,7 @@ import json
 import re
 from typing import List, Callable, TypeVar
 import datetime
+from abc import abstractmethod
 
 import nbformat
 from nbformat import NotebookNode
@@ -10,76 +11,105 @@ CellProcessor = Callable[[NotebookNode], None]
 
 
 class BaseUpdater:
-    cell_processors: List[CellProcessor]
+    cell_processors: List[CellProcessor] = []
 
-    def cell_processor(self, f: CellProcessor):
-        self.cell_processors.append(f)
+    def __init_subclass__(cls) -> None:
+        cls.cell_processors = []
+
+    @abstractmethod
+    def get_project(self):
+        pass
+
+    @classmethod
+    def _cell_processor(cls, f: CellProcessor):
+        cls.cell_processors.append(f)
         return f
+
+    def __init__(self) -> None:
+        self.project = self.get_project()
+        self.home = self.project.home
+
+    def walk_tiers(self):
+        yield self.home
+
+        for wp in self.home:
+            yield wp
+
+            for exp in wp:
+                yield exp
+
+                for smpl in exp:
+                    yield smpl
+                    
+
+def cell_processor(f):
+    return BaseUpdater._cell_processor(f)
 
 
 class V0_1to0_2(BaseUpdater):
-    def __init__(self):
+    
+    def get_project(self):
         from .. import env
-
         assert env.project
+        return env.project
+    
+    def __init__(self):
+        super().__init__()
 
-        self.home = env.project.home
-        self.cell_processors = []
+    @cell_processor
+    def fix_imports(cell):
+        text = cell["source"]
+        text = text.replace(
+            "from htools.exp import g", "from htools.scify import *"
+        )
+        cell["source"] = text
 
-        @self.cell_processor
-        def fix_imports(cell):
-            text = cell["source"]
-            text = text.replace(
-                "from htools.exp import g", "from htools.scify import *"
-            )
-            cell["source"] = text
+    @cell_processor
+    def fix_g_references(cell):
+        text = cell["source"]
 
-        @self.cell_processor
-        def fix_g_references(cell):
-            text = cell["source"]
+        def replace(match):
+            return f"env.{match.group(1)}"
 
-            def replace(match):
-                return f"env.{match.group(1)}"
+        cell["source"] = re.sub("g\.(.+)", replace, text)
 
-            cell["source"] = re.sub("g\.(.+)", replace, text)
+    @cell_processor
+    def replace_mksmpl(cell):
+        pattern = "%%mksmpl (.+)"
+        text = cell["source"]
 
-        @self.cell_processor
-        def replace_mksmpl(cell):
-            pattern = "%%mksmpl (.+)"
-            text = cell["source"]
+        match = re.search(pattern, text)
+        if match:
+            cell["source"] = f"smpl = env('{match.group(1)}')\nsmpl.header()"
 
-            match = re.search(pattern, text)
-            if match:
-                cell["source"] = f"smpl = env('{match.group(1)}')\nsmpl.header()"
+    @cell_processor
+    def replace_smplconc(cell):
+        text = cell["source"]
+        cell["source"] = text.replace("%%smplconc", "%%conc")
 
-        @self.cell_processor
-        def replace_smplconc(cell):
-            text = cell["source"]
-            cell["source"] = text.replace("%%smplconc", "%%conc")
+    @cell_processor
+    def replace_expconc(cell):
+        text = cell["source"]
+        cell["source"] = text.replace("%%expconc", "%%conc")
 
-        @self.cell_processor
-        def replace_expconc(cell):
-            text = cell["source"]
-            cell["source"] = text.replace("%%expconc", "%%conc")
+    @cell_processor
+    def fix_meta_store(cell):
+        text = cell["source"]
+        pattern = "smpl\.write_meta\('(.+)', (.+)\)"
 
-        @self.cell_processor
-        def fix_meta_store(cell):
-            text = cell["source"]
-            pattern = "smpl\.write_meta\('(.+)', (.+)\)"
+        def replace(match):
+            return f"smpl.{match.group(1)} = {match.group(2)}"
 
-            def replace(match):
-                return f"smpl.{match.group(1)} = {match.group(2)}"
+        cell["source"] = re.sub(pattern, replace, text)
 
-            cell["source"] = re.sub(pattern, replace, text)
-
-    def walk_nbs(self):
+    def walk_smpls(self):
         for wp in self.home:
             for exp in wp:
                 for smpl in exp:
                     yield smpl
 
     def update(self):
-        for smpl in self.walk_nbs():
+        for smpl in self.walk_smpls():
             if not smpl.file.exists():
                 print(f"No file found for {smpl} - skippiping")
                 continue
@@ -116,18 +146,13 @@ class V0_1to0_2(BaseUpdater):
 
 class V0_2to0_3(BaseUpdater):
 
-    def __init__(self) -> None:
+    def get_project(self):
         from .. import env
-
         assert env.project
+        return env.project
 
-        self.home = env.project.home
-
-    def walk_tiers(self):
-        for wp in self.home:
-            for exp in wp:
-                for smpl in exp:
-                    yield smpl
+    def __init__(self) -> None:
+        super().__init__()
 
     def update(self):
         from cassini import NotebookTierBase
@@ -135,19 +160,18 @@ class V0_2to0_3(BaseUpdater):
         for tier in self.walk_tiers():
             if not isinstance(tier, NotebookTierBase):
                 continue
-            
-            with open(tier.meta_file, 'w') as fs:
-                meta = json.load(fs)
+            else:
+                self.update_meta(tier)
 
-            started = meta.get('started')
+    def update_meta(self, tier):
+        with open(tier.meta_file, 'r') as fs:
+            meta = json.load(fs)
 
-            if started:
-                started_dt = datetime.datetime.strptime(started, "%d/%m/%Y")
-                meta['started'] = str(started_dt)
-            
-            with open(tier.meta_file, 'w') as fs:
-                json.dump(meta, fs)
+        started = meta.get('started')
 
-
-
-                
+        if started:
+            started_dt = datetime.datetime.strptime(started, "%d/%m/%Y")
+            meta['started'] = str(started_dt)
+        
+        with open(tier.meta_file, 'w') as fs:
+            json.dump(meta, fs)
---
+++ tests/compat/test_update.py
@@ -0,0 +1,65 @@
+import shutil
+import json
+import datetime
+
+import pytest
+
+from cassini.compat.update import V0_2to0_3
+from cassini.utils import find_project
+from cassini import env
+from cassini.core import NotebookTierBase
+
+
+@pytest.fixture
+def get_migrator(tmp_path, monkeypatch):
+    shutil.copytree('tests/compat/0.2.0', tmp_path, dirs_exist_ok=True)
+    monkeypatch.setenv('CASSINI_PROJECT', tmp_path / 'project.py')
+    
+    find_project()
+    yield V0_2to0_3()
+    
+    env._reset()
+
+
+def test_get_project(get_migrator):
+    migrator = get_migrator
+    assert migrator.project
+
+
+def test_iterate(get_migrator):
+    migrator = get_migrator
+    
+    all_names = set([
+        'Home',
+        'WP1',
+        'WP1.1',
+        'WP1.2',
+        'WP1.1a',
+        'WP1.1b',
+        'WP2',
+        'WP2.2',
+        'WP2.2a'
+    ])
+
+    for tier in migrator.walk_tiers():
+        all_names.remove(tier.name)
+
+    assert len(all_names) == 0
+
+
+def test_update_meta(get_migrator):
+    migrator = get_migrator
+    WP1 = migrator.project['WP1.1']
+    assert '/' in json.loads(WP1.meta_file.read_text())['started']
+    migrator.update_meta(WP1)
+    assert '/' not in json.loads(WP1.meta_file.read_text())['started']
+    assert isinstance(WP1.started, datetime.datetime)
+
+
+def test_full_migrate(get_migrator):
+    migrator = get_migrator
+    migrator.update()
+
+    for tier in migrator.walk_tiers():
+        if isinstance(tier, NotebookTierBase):
+            assert isinstance(tier.started, datetime.datetime)
