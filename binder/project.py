ae7a38702f0f8b768e2e46a87e49680717da1ce8
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2024-08-26 19:39:21 +0100

    cas_project.py-ification

--- binder/project.py
+++
@@ -1,9 +0,0 @@
-from cassini import DEFAULT_TIERS
-from cassini.core import Project
-from cassini.jlgui import extend_project
-
-project = Project(DEFAULT_TIERS, __file__)
-extend_project(project)
-
-if __name__ == '__main__':
-    project.launch()
